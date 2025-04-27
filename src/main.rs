17d110f8f0529e54b732dba3ac515f68d94a8ae2
Kurandur <4482510+Kurandur@users.noreply.github.com>
2025-04-27 18:00:25 +0200

    ✨feat(Lesson 1): Add first line function implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::f32::consts::PI;
-
 use tinyrenderer_rust::tga::{Format, TGAColor, TGAImage};
 
 const IMAGE_WIDTH: i32 = 100;
@@ -9,7 +7,16 @@ fn main() {
     let mut image = TGAImage::new(IMAGE_WIDTH, IMAGE_HEIGHT, Format::RGB);
     let magenta = TGAColor::from_rgba(255, 0, 255, 255);
 
-    image.set(52, 41, magenta).unwrap();
+    line(13, 20, 80, 40, &mut image, magenta);
 
     image.write_tga_file("output.tga", true, true).unwrap();
+}
+
+fn line(x0: i32, y0: i32, x1: i32, y1: i32, image: &mut TGAImage, color: TGAColor) {
+    for i in 0..=100 {
+        let t = i as f32 / 100.0;
+        let x = x0 + ((x1 - x0) as f32 * t) as i32;
+        let y = y0 + ((y1 - y0) as f32 * t) as i32;
+        image.set(x as usize, y as usize, color).unwrap();
+    }
 }
